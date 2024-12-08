6d25813c175237f1d6d95cd49f9c1d9c5e3db746
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2024-12-08 08:40:59 +0300

    Update Tr.py

--- YukkiMusic/plugins/tools/Tr.py
+++ YukkiMusic/plugins/tools/Tr.py
@@ -7,9 +7,9 @@ from YukkiMusic import app
 def get_current_time():
     tz = pytz.timezone('Asia/Riyadh')  # Setting the timezone to India (Kolkata)
     current_time = datetime.now(tz)
-    return current_time.strftime("%Y-%m-%d \n %H:%M:%S %Z%z")
+    return current_time.strftime("%Y-%m-%d \n\n %H:%M:%S \n\n %Z%z")
 
 @app.on_message(filters.command(["Time"]))
 def send_time(client, message):
     time = get_current_time()
-    client.send_message(message.chat.id, f"Current time in India: {time}")  
+    client.send_message(message.chat.id, f"Current time in India\n: {time}")  
